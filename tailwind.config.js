c96d216fc0dd7c580a544feaac4aadaae7374cfa
jerichocalibuso <jerichocalibuso@gmail.com>
2022-01-22 10:19:56 +0800

    fix: build settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  mode: "jit",
+  purge: [
+    "./public/**/*.html",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
     extend: {},
