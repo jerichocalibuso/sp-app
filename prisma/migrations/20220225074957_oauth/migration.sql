c661b1039130d992037ecc72c84bd3516764c9f2
jerichocalibuso <jerichocalibuso@gmail.com>
2022-02-25 15:58:43 +0800

    fix: reinitialize migration db

--- prisma/migrations/20220225074957_oauth/migration.sql
+++ prisma/migrations/20220225074957_oauth/migration.sql
@@ -1,24 +1,34 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'RIDER', 'CUSTOMER', 'GUEST');
 
-
+-- CreateEnum
 CREATE TYPE "Status" AS ENUM ('PACKAGING', 'OUT_FOR_DELIVERY', 'COMPLETED', 'CANCELLED');
 
-
+-- CreateEnum
 CREATE TYPE "PaymentOption" AS ENUM ('COD', 'CARD', 'GCASH', 'PAYMAYA', 'GRABPAY');
 
-
+-- CreateEnum
 CREATE TYPE "Category" AS ENUM ('PORK', 'CHICKEN', 'BEEF');
 
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "role" "Role" NOT NULL,
+    "username" TEXT,
+    "email" TEXT,
+    "provider" TEXT,
+    "providerId" TEXT,
+    "password" TEXT,
+    "contactNumber" TEXT,
+    "name" TEXT,
+    "profileIconUrl" TEXT,
 
-ALTER TYPE "Role" ADD VALUE 'GUEST';
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
 
-
-ALTER TABLE "User" DROP COLUMN "content",
-ADD COLUMN     "contactNumber" TEXT,
-ADD COLUMN     "name" TEXT,
-ADD COLUMN     "password" TEXT,
-ADD COLUMN     "username" TEXT;
-
-
+-- CreateTable
 CREATE TABLE "Address" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -33,7 +43,7 @@ CREATE TABLE "Address" (
     CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
 );
 
-
+-- CreateTable
 CREATE TABLE "Order" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -47,7 +57,7 @@ CREATE TABLE "Order" (
     CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
 );
 
-
+-- CreateTable
 CREATE TABLE "Product" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -59,16 +69,21 @@ CREATE TABLE "Product" (
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );
- CreateIndex
-CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
- CreateIndex
+
+-- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "User_providerId_key" ON "User"("providerId");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
+
+-- AddForeignKey
 ALTER TABLE "Address" ADD CONSTRAINT "Address_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
-
+-- AddForeignKey
 ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
-
+-- AddForeignKey
 ALTER TABLE "Product" ADD CONSTRAINT "Product_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE SET NULL ON UPDATE CASCADE;
