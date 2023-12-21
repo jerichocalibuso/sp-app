952681c1bf7bed709e83fde126dda88bde2f2ad5
jerichocalibuso <jerichocalibuso@gmail.com>
2023-12-21 18:58:30 +0800

    fix: dependencies

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,21 +1,23 @@
-import { renderToString } from "react-dom/server";
-import { RemixServer } from "remix";
-import type { EntryContext } from "remix";
+import type { EntryContext } from '@remix-run/node'
+import { RemixServer } from '@remix-run/react'
+import { renderToString } from 'react-dom/server'
 
-export default function handleRequest(
+export const handleRequest = (
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
-) {
+) => {
   const markup = renderToString(
     <RemixServer context={remixContext} url={request.url} />
-  );
+  )
 
-  responseHeaders.set("Content-Type", "text/html");
+  responseHeaders.set('Content-Type', 'text/html')
 
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: responseStatusCode,
+  return new Response('<!DOCTYPE html>' + markup, {
     headers: responseHeaders,
-  });
+    status: responseStatusCode,
+  })
 }
+
+export default handleRequest
